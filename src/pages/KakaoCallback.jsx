4d9136322452c773e12b3e7702d5fa6019dcbd92
judith <8w8u8@naver.com>
2025-07-30 07:01:53 +0900

    feat: 스피너 추가

--- src/pages/KakaoCallback.jsx
+++ src/pages/KakaoCallback.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import SuspenseLoading from "../../components/common/SuspenseLoading";
+import SuspenseLoading from "../components/common/SuspenseLoading";
 
 export default function KakaoCallback() {
   const navigate = useNavigate();
